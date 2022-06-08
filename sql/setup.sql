a449f4f6c65a3606ab3058c8afb5864469abcd5f
JeffAllison <itsjallison@gmail.com>
2022-06-08 16:47:18 -0700

    added table in setup.sql file and setup db in beekeeper studio

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,27 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP table if exists amps; 
+
+CREATE table amps (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR NOT NULL,
+  manufacturer VARCHAR NOT NULL,
+  year_released INT NOT NULL,
+  watts INT NOT NULL,
+  reissued BOOLEAN
+);
+
+INSERT INTO amps (name, manufacturer, year_released, watts, reissued) VALUES
+
+('Twin Reverb', 'Fender', 1963, 85, true),
+
+('Super Lead', 'Marshall', 1959, 100, true),
+
+('OR120', 'Orange', 1972, 120, false),
+
+('AC30', 'Vox', 1958, 30, true),
+
+('YSR-1 Custom Reverb', 'Traynor', 1968, 45, false),
+
+('JC50', 'Roland', 1980, 50, false);
