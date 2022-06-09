4af779131271b2cdf204843f543e79aa2c241cfd
JeffAllison <itsjallison@gmail.com>
2022-06-08 17:18:47 -0700

    created Amp class in models Amp.js file, created model constructor for amp table, created getAll and getById functions to pull data from amp table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Amp = require('..lib/models/Amp');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
