95b39a2af219f42a594d915fdce1707837942f81
JeffAllison <itsjallison@gmail.com>
2022-06-08 18:00:05 -0700

    created tests for route to mapped list and route to single list item by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,14 +2,33 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const Amp = require('..lib/models/Amp');
+const Amp = require('../lib/models/Amp');
 
-describe('backend-express-template routes', () => {
+describe('amp routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/amps should return the list of amps and values', async () => {
+    const res = await request(app).get('/amps');
+    const amps = await Amp.getAll();
+    const expected = amps.map((amp) => {
+      return { id: amp.id, name: amp.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/amps/:id should return single amp detail', async () => {
+    const res = await request(app).get('/amps/5');
+    const traynor = {
+      id: 5,
+      name: 'YSR-1 Custom Reverb',
+      manufacturer: 'Traynor',
+      year_released: 1968,
+      watts: 45,
+      reissued: false,
+    };
+    expect(res.body).toEqual(traynor);
   });
   afterAll(() => {
     pool.end();
